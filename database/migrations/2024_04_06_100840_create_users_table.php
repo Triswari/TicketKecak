83510bdfdd2a7e00e93a2844b409164e83481314
Triswari <96421556+Triswari@users.noreply.github.com>
2024-07-30 23:04:46 +0800

    Update 2024_04_06_100840_create_users_table.php

--- database/migrations/2024_04_06_100840_create_users_table.php
+++ database/migrations/2024_04_06_100840_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('firstname')->nullable();
             $table->string('lastname')->nullable();
             $table->string('email')->unique();
+            $table->string('google_id')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('address')->nullable();
